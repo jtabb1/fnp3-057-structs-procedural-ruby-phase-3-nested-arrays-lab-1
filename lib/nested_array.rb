dab7b12a37ec91b3fede3e1982491e0bea54afde
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-06 07:02:36 -0400

    Complete assignment fc v.01

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -16,26 +16,41 @@ CONVENTIONAL_PRODUCE = [
   "Eggplant"
 ]
 
+# ma = ORGANIC_PRODUCE
+# mb = CONVENTIONAL_PRODUCE
+
 def assembled_matrix
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
+  [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
 end
+puts assembled_matrix.inspect
 
 def sorted_matrix
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
+  [CONVENTIONAL_PRODUCE.sort{|a,b| a[0] <=> b[0]}, CONVENTIONAL_PRODUCE.sort{|a,b| a[0] <=> b[0]}]
 end
+puts sorted_matrix.inspect
 
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  matrix[row][column]
 end
+puts matrix_lookup(assembled_matrix,1,2)
 
 def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+  matrix[row][column] = new_value
+  matrix
 end
+puts matrix_update(assembled_matrix,0,1,'x').inspect
+puts matrix_update(assembled_matrix,1,2,'y').inspect
+puts CONVENTIONAL_PRODUCE.inspect
+puts ORGANIC_PRODUCE.inspect
---
+++ text.zsh
@@ -0,0 +1,27 @@
+jeremiahtabb@Jeremiahs-Air fnp3-057-structs-procedural-ruby-phase-3-nested-arrays-lab-1 % rspec
+
+An error occurred while loading ./spec/nested_array_spec.rb.
+Failure/Error: require 'bundler'
+
+Gem::MissingSpecVersionError:
+  Could not find 'bundler' (1.16.6) required by your /Users/jeremiahtabb/Development/Code/FnWork2021/3phase3/fnp3-057-structs-procedural-ruby-phase-3-nested-arrays-lab-1/Gemfile.lock.
+  To update to the latest version installed on your system, run `bundle update --bundler`.
+  To install the missing version, run `gem install bundler:1.16.6`
+  Checked in 'GEM_PATH=/Users/jeremiahtabb/.rvm/gems/ruby-2.7.3:/Users/jeremiahtabb/.rvm/rubies/ruby-2.7.3/lib/ruby/gems/2.7.0' , execute `gem env` for more information
+# ./spec/spec_helper.rb:1:in `<top (required)>'
+# ./spec/nested_array_spec.rb:1:in `<top (required)>'
+No examples found.
+
+
+Finished in 0.00002 seconds (files took 0.09136 seconds to load)
+0 examples, 0 failures, 1 error occurred outside of examples
+
+jeremiahtabb@Jeremiahs-Air fnp3-057-structs-procedural-ruby-phase-3-nested-arrays-lab-1 % bundle install                     Traceback (most recent call last):
+        4: from /Users/jeremiahtabb/.rvm/gems/ruby-2.7.3/bin/ruby_executable_hooks:22:in `<main>'
+        3: from /Users/jeremiahtabb/.rvm/gems/ruby-2.7.3/bin/ruby_executable_hooks:22:in `eval'
+        2: from /Users/jeremiahtabb/.rvm/gems/ruby-2.7.3/bin/bundle:23:in `<main>'
+        1: from /Users/jeremiahtabb/.rvm/rubies/ruby-2.7.3/lib/ruby/site_ruby/2.7.0/rubygems.rb:300:in `activate_bin_path'
+/Users/jeremiahtabb/.rvm/rubies/ruby-2.7.3/lib/ruby/site_ruby/2.7.0/rubygems.rb:281:in `find_spec_for_exe': Could not find 'bundler' (1.16.6) required by your /Users/jeremiahtabb/Development/Code/FnWork2021/3phase3/fnp3-057-structs-procedural-ruby-phase-3-nested-arrays-lab-1/Gemfile.lock. (Gem::GemNotFoundException)
+To update to the latest version installed on your system, run `bundle update --bundler`.
+To install the missing version, run `gem install bundler:1.16.6`
+jeremiahtabb@Jeremiahs-Air fnp3-057-structs-procedural-ruby-phase-3-nested-arrays-lab-1 % 
